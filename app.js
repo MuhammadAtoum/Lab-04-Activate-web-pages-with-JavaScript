babc3625d15d8f243d9720622f909cc5c44c9b20
MuhammadAtoum <muhammadatoum@gmail.com>
2021-03-06 16:13:19 +0200

    Lab-04-Activate-web-pages-with-JavaScript

---
+++ app.js
@@ -0,0 +1,13 @@
+
+alert("wellcome to my new page"); 
+alert("do you love porograming")
+alert("you should enter your name and your password")
+var the_name = prompt("please enter your name ")
+if(the_name = String){alert("will done");}
+else{alert("not matched")}
+document.write(the_name)
+console.log(the_name);
+var password = prompt("please enter you new password")
+confirm(password)
+document.write(password)
+console.log(password);
